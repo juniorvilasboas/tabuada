b643b57d36ca43d3510018bdfb83eb7baedb7cb8
Moacyr Santana <junior.vilasboas@gmail.com>
2021-12-02 09:57:59 +0000

    Atualizando arquivo index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 const indexRouter = require('./routes/index')
 const tabuadaRouter = require('./routes/tabuada')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
